2aacffada902ac55e4748e7adde73403565bf562
Vladimir Golovnin <thyrotron@gmail.com>
2025-03-16 11:02:03 +0100

    Module-4: Implemented Order aggregate.

---
+++ core/src/main/kotlin/me/vgolovnin/ddd/delivery/core/domain/model/courier/Courier.kt
@@ -0,0 +1,7 @@
+package me.vgolovnin.ddd.delivery.core.domain.model.courier
+
+import java.util.*
+
+class Courier {
+    val id: UUID = UUID.randomUUID()
+}
---
+++ core/src/main/kotlin/me/vgolovnin/ddd/delivery/core/domain/model/order/OrderStatus.kt
@@ -0,0 +1,7 @@
+package me.vgolovnin.ddd.delivery.core.domain.model.order
+
+enum class OrderStatus {
+    CREATED,
+    ASSIGNED,
+    COMPLETED,
+}
