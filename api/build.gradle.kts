c79d03661dc4006cb6d53cd80671f2ea32bf4b8d
Vladimir Golovnin <thyrotron@gmail.com>
2025-03-31 21:55:40 +0200

    Module-8: Configured OpenAPI generator.

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("plugin.spring") version "2.1.0"
     id("org.springframework.boot") version "3.4.4"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.openapi.generator") version "7.12.0"
 }
 
 group = "me.vgolovnin.ddd.delivery"
@@ -20,10 +21,10 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
     implementation("dev.ceviz:spring-3x-kordinator:1.0.0")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
 
     testImplementation(kotlin("test"))
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-
 }
 
 tasks.test {
@@ -31,4 +32,28 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(17)
+}
+
+openApiGenerate {
+    inputSpec = "openapi.yml"
+    generatorName = "kotlin-spring"
+    outputDir = "$projectDir"
+    generateApiTests = false
+    generateModelTests = false
+    generateApiDocumentation = false
+    generateModelDocumentation = false
+    ignoreFileOverride = "$projectDir/.openapi-generator-ignore"
+    supportingFilesConstrainedTo = listOf()
+    modelPackage = "me.vgolovnin.ddd.delivery.api.adapters.http"
+    apiPackage = "me.vgolovnin.ddd.delivery.api.adapters.http"
+    configOptions = mapOf(
+        "basePackage" to "me.vgolovnin.ddd.delivery.api.config",
+        "interfaceOnly" to "true",
+        "exceptionHandler" to "false",
+        "gradleBuildFile" to "false",
+        "pomFile" to "false",
+        "skipDefaultInterface" to "true",
+        "useSpringBoot3" to "true",
+        "useTags" to "true",
+    )
 }
