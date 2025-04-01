eac3ffd5df83074462c1848b27e5c01138bed5fb
Vladimir Golovnin <thyrotron@gmail.com>
2025-04-01 09:56:41 +0200

    Module-8: Implemented "get couriers" REST API endpoint.

--- api/src/main/kotlin/me/vgolovnin/ddd/delivery/api/adapters/http/DeliveryApiImpl.kt
+++ api/src/main/kotlin/me/vgolovnin/ddd/delivery/api/adapters/http/DeliveryApiImpl.kt
@@ -3,6 +3,7 @@ package me.vgolovnin.ddd.delivery.api.adapters.http
 import dev.ceviz.Mediator
 import kotlinx.coroutines.runBlocking
 import me.vgolovnin.ddd.delivery.core.application.usecases.command.CreateOrderCommand
+import me.vgolovnin.ddd.delivery.core.application.usecases.query.AllCouriersQuery
 import org.apache.commons.lang3.RandomStringUtils
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
@@ -18,11 +19,12 @@ class DeliveryApiImpl(
         val orderId = UUID.randomUUID()
         val street = RandomStringUtils.insecure().nextPrint(20)
         mediator.send(CreateOrderCommand(orderId, street))
-        ResponseEntity.created( URI.create("/orders/$orderId")).build()
+        ResponseEntity.created(URI.create("/orders/$orderId")).build()
     }
 
-    override fun getCouriers(): ResponseEntity<List<Courier>> {
-        TODO("Not yet implemented")
+    override fun getCouriers(): ResponseEntity<List<Courier>> = runBlocking {
+        val couriers = mediator.send(AllCouriersQuery).couriers
+        ResponseEntity.ok(couriers.map { Courier(it.id, it.name, Location(it.location.x, it.location.y)) })
     }
 
     override fun getOrders(): ResponseEntity<List<Order>> {
