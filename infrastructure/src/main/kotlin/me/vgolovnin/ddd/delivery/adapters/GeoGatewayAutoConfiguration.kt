70222d5733bfa18519825b4d81de9d0278c463a1
Vladimir Golovnin <thyrotron@gmail.com>
2025-04-03 08:48:46 +0200

    Module-9: Final implementation of geo-service client.

--- infrastructure/src/main/kotlin/me/vgolovnin/ddd/delivery/adapters/GeoGatewayAutoConfiguration.kt
+++
@@ -1,8 +0,0 @@
-package me.vgolovnin.ddd.delivery.adapters
-
-import org.springframework.boot.autoconfigure.AutoConfiguration
-import org.springframework.context.annotation.ComponentScan
-
-@AutoConfiguration
-@ComponentScan(basePackages = ["me.vgolovnin.ddd.delivery.adapters.grpc"])
-internal class GeoGatewayAutoConfiguration
--- infrastructure/src/main/kotlin/me/vgolovnin/ddd/delivery/adapters/grpc/GeoClient.kt
+++ infrastructure/src/main/kotlin/me/vgolovnin/ddd/delivery/adapters/grpc/GeoClient.kt
@@ -1,26 +1,21 @@
 package me.vgolovnin.ddd.delivery.adapters.grpc
 
-import geo.GeoGrpc
+import geo.GeoGrpc.GeoBlockingStub
 import geo.GeoOuterClass
-import io.grpc.ManagedChannelBuilder
 import me.vgolovnin.ddd.delivery.core.domain.sharedkernel.Location
 import me.vgolovnin.ddd.delivery.core.ports.GeoGateway
 import org.springframework.stereotype.Component
-import java.util.concurrent.TimeUnit
 
 @Component
-internal class GeoClient : GeoGateway {
+internal class GeoClient(
+    private val geoServiceStub: GeoBlockingStub
+) : GeoGateway {
 
     override fun findStreetLocation(street: String): Location {
-        val channel = ManagedChannelBuilder.forTarget("localhost:5004").usePlaintext().build()
-        val stub = GeoGrpc.newBlockingStub(channel)
+        val request = GeoOuterClass.GetGeolocationRequest.newBuilder()
+            .setStreet(street).build()
 
-        val request = GeoOuterClass.GetGeolocationRequest.newBuilder().setStreet("123").build()
-
-        val response = stub.getGeolocation(request)
-        val location = response.location
-        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS)
-
-        return Location(location.x, location.y)
+        return geoServiceStub.getGeolocation(request).location
+            .run { Location(x, y) }
     }
 }
